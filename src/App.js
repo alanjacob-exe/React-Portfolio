36ae88397b154de911194bc6ad0453d242508c98
alanjacob-exe <77325299+alanjacob-exe@users.noreply.github.com>
2022-09-11 20:39:04 +0530

    Animateed Text
    Added Animated Text on home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 import './app.scss'
+import Home from './Components/Home';
 import Layout from './Components/Layout/Layout';
+
+
+
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout/>}/>
+      <Route path="/" element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      </Route>
     </Routes>
     </>
   )
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,17 +1,52 @@
-import logo from '../../assets/images/logo-s.png';
-import './index.scss'
+import logoTitle from "../../assets/images/logo-s.png";
+import "./index.scss";
+import { Link } from 'react-router-dom';
+import { useState } from "react";
+import AnimatedLetters from "../AnimatedLetter";
+import { useEffect } from "react";
+import Logo from '../Logo';
+
 
 const Home = () => {
-    return(
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>Hi,<br/> Im Alan
-                <img src={logo} alt="developer"/>
-                Fresher</h1>
-                <h2>Student / React Developer</h2> 
-                <Link to="/contact" className="flat-button">Contact Me</Link>
-            </div>
-        </div>
-    );
-}
+
+  const[letterClass,setLetterClass]=useState('text-animate')
+  const nameArray=['l','a','n']
+  const jobArray=['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    },4000)
+    
+  }, [])
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i</span>
+
+
+          <br />
+          <span className={`${letterClass} _13`}>i</span>
+          <span className={`${letterClass} _14`}>'m</span>
+
+
+          <img src={logoTitle} alt="developer" />
+          <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+          <br />
+          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22}/>
+
+        </h1>
+        <h2>Student / React Developer</h2>
+        <Link to="/contact" className="flat-button">Contact Me</Link>
+
+
+
+      </div>
+      <Logo></Logo>
+    </div>
+  );
+};
 export default Home;
